f822b9855f4fc6f181914558990b358490fc8863
Sebastian Jug <seb@stianj.ug>
2021-11-29 11:48:05 -0500

    Modify scripts for SBDB auto deployment

--- OCP-4.X/roles/post-install/files/sbdb-relay.sh
+++ OCP-4.X/roles/post-install/files/sbdb-relay.sh
@@ -7,19 +7,28 @@
 OVN_NS=openshift-ovn-kubernetes
 
 if [[ "$1" == "deploy" ]]; then
-  OVNMASTER_PODS=($(oc get po -o wide -n ${OVN_NS} | awk '/ovnkube-master/{ print $1 }'))
+  FOUND=0
 
   echo "Deploying SBDB"
-  for POD in "${OVNMASTER_PODS[@]}"; do
-      oc logs ${POD} -c ovnkube-master | grep relay.go
-      if [[ $? -eq 0 ]]; then
-        echo "Found ovnkube-master leader: ${POD}"
-        YAML=$(oc logs ${POD} -c ovnkube-master | awk '/\-\-\-/,/^\w[0-9]{4}/' | head -n -1)
-        echo "${YAML}" | oc create -f -
-        break
-      else
-        echo "${POD} does not contain relay yaml"
-      fi
+  while [ ${FOUND} -lt 1 ]; do
+  OVNMASTER_PODS=($(oc get po -o wide -n ${OVN_NS} | awk '/ovnkube-master/{ print $1 }'))
+    echo "Found pods: ${OVNMASTER_PODS[*]}"
+    for POD in "${OVNMASTER_PODS[@]}"; do
+        oc logs ${POD} -c ovnkube-master | grep relay.go
+        if [[ $? -eq 0 ]]; then
+          echo "Found ovnkube-master leader: ${POD}"
+          YAML=$(oc logs ${POD} -c ovnkube-master | awk '/\-\-\-/,/^\w[0-9]{4}/' | head -n -1)
+          echo "${YAML}" | oc create -f -
+	  FOUND=1
+          break
+        else
+          echo "${POD} does not contain relay yaml."
+        fi
+    done
+    if [ ${FOUND} -eq 0 ]; then
+      echo "Unable to find relay yaml this loop, retying in 30s."
+      sleep 30
+    fi
   done
 elif [[ "$1" == "verify" ]]; then
   SBDB_IPS=($(oc get po -o wide -n ${OVN_NS} | awk '/sbdb-relay/{ print $6 }'))
@@ -28,7 +37,7 @@ elif [[ "$1" == "verify" ]]; then
   NODE_COUNT=${#OVNKUBE_PODS[@]}
   declare -A ready
   
-  echo "SBDB IPs: ${SBDB_IPS_STR}"
+  echo "SBDB relay pod IPs: ${SBDB_IPS_STR}"
 
   # Populate array tracking node readiness state
   for PODS in "${OVNKUBE_PODS[@]}"; do
@@ -38,6 +47,7 @@ elif [[ "$1" == "verify" ]]; then
   # Check SBDB connection of each ovn-controller
   READY_NODES=0
   while [ "${READY_NODES}" -lt "${NODE_COUNT}" ] ; do
+    printf "\nStarting SBDB IP search.\n"
     for POD in "${OVNKUBE_PODS[@]}"; do
       if ! ${ready[${POD}]}; then
         echo "Searching pod ${POD} for SBDB IPs."
@@ -47,6 +57,8 @@ elif [[ "$1" == "verify" ]]; then
           echo "Found ${POD} using relays (Ready ${READY_NODES}/${NODE_COUNT})."
           ready[${POD}]=true
           break
+	else
+          echo "${POD} isn't using the SBDB relay(s)."
         fi
       fi
     done
